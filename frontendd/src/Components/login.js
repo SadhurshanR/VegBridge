c9ca01d879b1d99329462a36795edfb6e82cd46e
SadhurshanR <142770513+SadhurshanR@users.noreply.github.com>
2024-12-08 01:56:55 +0530

    Update login.js

--- frontendd/src/Components/login.js
+++ frontendd/src/Components/login.js
@@ -18,7 +18,7 @@ const LoginSection = ({ onLoginSuccess }) => {
     setErrors({});
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
+      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
