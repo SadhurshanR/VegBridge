f42427a8dbd83f610607b0cf18f6abafd4726385
SadhurshanR <142770513+SadhurshanR@users.noreply.github.com>
2024-12-07 22:02:07 +0530

    Update index.js

--- frontendd/src/index.js
+++ frontendd/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { v7_relativeSplatPath } from 'react-router-dom/future';
 import './index.css';
 import App from './App';
+
+const router = createBrowserRouter({
+  future: { v7_relativeSplatPath },
+  routes: [
+    {
+      path: '/',
+      element: <App />,
+      // add your other routes here
+    },
+    // Add other routes as needed
+  ],
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
